13f514a4d50147a5316173ed17ea38ffd8c7d7f2
balickim <stiekerosiem@gmail.com>
2020-04-20 13:17:06 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+include_once 'databaseConnection.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$stat = pg_connection_status($db);
+  if ($stat === PGSQL_CONNECTION_OK) {
+      echo 'Connection status ok';
+  } else {
+      echo 'Connection status bad';
+  } 
+  
+echo '</br>';
+
+$result = pg_query($db, "SELECT trainingid, poolid FROM trainings");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+echo "<script>console.log('Debug Objects: " . $result . "' );</script>";
+
+while ($row = pg_fetch_row($result)) {
+  echo "name: $row[0]  surname: $row[1]";
+  echo "<br />\n";
+}
+
+?>
