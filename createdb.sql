ac146db03fbf0b00445188c472ebdb6c471846f0
balickim <stiekerosiem@gmail.com>
2020-05-11 11:14:30 +0200

    modify db creation script

--- createdb.sql
+++ createdb.sql
@@ -8,17 +8,4 @@ create table trainings (
 	description VARCHAR(50)
 );
 
-create table pools (
-	poolid INT PRIMARY KEY,
-	length INT,
-	name VARCHAR(50)
-);
-
-insert into pools (poolid, length, name) values (0, 0, '-');
-insert into pools (poolid, length, name) values (1, 25, 'small');
-insert into pools (poolid, length, name) values (2, 50, 'big');
-
-ALTER TABLE trainings
-	ADD FOREIGN KEY (poolid) REFERENCES pools (poolid);
-
 COMMIT;
